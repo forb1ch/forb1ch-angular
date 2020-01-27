3daf85d4847de4000f701141a4811efbbaa75187
Illia <illia.ostrovskyi@gmail.com>
2020-01-27 17:42:45 +0200

    acquaintance with angular routing, api, components etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ApiServicesService } from './api-services.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: `./app.component.html`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'forb1ch-angular';
+  userId: number;
+  user: any;
+
+  constructor(private http: HttpClient) {
+  }
+  search(id) {
+    this.http.get('http://5e0f48f09576aa0014666536.mockapi.io/Angular-study-json/' + this.userId)
+      .subscribe((data) => {
+        this.user = data;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { DateComponent } from './date/date.component';
+import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServicesComponent } from './services/services.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoComponent } from './todo/todo.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ApiServicesService } from './api-services.service';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'todo', component: TodoComponent},
+  {path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
+    ServicesComponent,
+    HeaderComponent,
+    TodoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiServicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
